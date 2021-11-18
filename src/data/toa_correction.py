b705f319ae1abde966d3013f0131f0260b29b8d5
Nakita Oza <ozanakita@gmail.com>
2021-11-18 14:54:38 +0100

    Update toa_correction.py

--- src/data/toa_correction.py
+++ src/data/toa_correction.py
@@ -5,7 +5,8 @@ import subprocess
 def toa_correct_dataset(params):
     print('IN TOA CORRECT')
     data_path = params.project_path + "data/raw/SPARCS_dataset/"
-    data_output_path = params.project_path + "data/processed/SPARCS_TOA/"
+    data_toa_path = params.project_path + "data/processed/SPARCS_TOA/"
+    data_output_path = params.project_path + "data/output/SPARCS/"
     products = sorted(os.listdir(data_path))
     products = [p for p in products if 'data.tif' in p]
     print('IN TOA CORRECT 1, length', len(products))
@@ -43,9 +44,9 @@ def toa_correct_dataset(params):
         #
         # # ## CREATE LIST of FILE NAME FRO GDAL_MERGE
         subprocess.run('dir /b /s *B?.tif > list.txt', shell=True, cwd=data_output_path)
-        print('IN TOA CORRECT 4')
+        print('IN TOA CORRECT 41')
         subprocess.run('dir /b /s *B10.tif *B11.tif> list2.txt', shell=True, cwd=data_output_path)
-        print('IN TOA CORRECT 5')
+        print('IN TOA CORRECT 51')
 
         # # Run Fmask -GDAL
         c5 = data_output_path[:-1] + '/ref.img'
@@ -87,13 +88,12 @@ def toa_correct_dataset(params):
         print('IN TOA CORRECT 8')
 
         # Translate to .tif file
-        p3 = data_output_path[:-1] + '/' + product[0:24] + '_toa.TIF'
+        p3 = data_toa_path[:-1] + '/' + product[0:24] + '_toa.TIF'
         cmd = [p1, '-of', 'Gtiff', d4, p3]
         subprocess.run(cmd)
         print('IN TOA CORRECT 9')
 
         # Delete temp files
-        subprocess.run('del *.tif *.img *.xml', shell=True, cwd=data_output_path)
+        subprocess.run('del *.tif *.img *.xml *.txt', shell=True, cwd=data_output_path)
         print('IN TOA CORRECT 10')
 
-        break
